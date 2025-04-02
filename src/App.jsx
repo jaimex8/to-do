ceb52644552c686b31b590c5b2d07f71b93cbe32
Jaime Sandoval <JSan88@protonmail.com>
2025-04-02 15:26:11 -0700

    add state to input

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
 import { useState } from 'react'
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 import Todo from './components/Todo.jsx';
 import './App.css'
 
-function App() {
+function App(props) {
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks?.map((task) => (
+    <Todo 
+    id={task.id} 
+    name={task.name} 
+    completed={task.completed}
+    key={task.id} 
+    />
+  ));
+
+  function addTask(name) {
+    const newTask = { id: "id", name, completed: false };
+    setTasks([tasks, newTask]);
+  }
+
 
   return (
     <>      
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
-        <Todo name="Eat" id="todo-0" completed />
-        <Todo name="Sleep" id="todo-1" />
-        <Todo name="Repeat" id="todo-2" />
-      </ul>
-
+      <div className="todoapp stack-large">
+        <h1>Todo</h1>
+        <Form addTask={addTask} />
+        <div className="filters btn-group stack-exception">
+          <FilterButton />
+          <FilterButton />
+          <FilterButton />
+        </div>
+        <h2 id="list-heading">3 tasks remaining</h2>
+        <ul
+          role="list"
+          className="todo-list stack-large stack-exception"
+          aria-labelledby="list-heading">
+          {taskList}
+        </ul>
+      </div>
     </>
   )
 }
